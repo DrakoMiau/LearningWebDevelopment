e4f09b13c34e5c5f6bd8ead899fc9581c7b48aca
Drako31 <debernalg@unal.edu.co>
2023-04-08 13:32:34 -0500

    04-03-2023

--- JavaScript/Leccion15/15-01-Promesas-UJS.js
+++ JavaScript/Leccion15/15-01-Promesas-UJS.js
@@ -7,7 +7,42 @@ let miPromesa = new Promise((resolved,rejected) =>{ //una promeda recibe dos par
         rejected('Se produjo un error');
 });
 
-miPromesa.then(
+/*miPromesa.then(  //aclaracion es un metodo de la clase Promise que recibe dos parametros de tipo flecha en 2 lineas
     valor => console.log(valor),
     error => console.log(error) 
-);
+);*/
+
+let promesa = new Promise((resolver) => {
+    setTimeout(() => resolver('saludos con promesa y timeout'), 3000)
+});
+
+//promesa.then(valor => console.log(valor));
+
+//async indica que una funcion regresa una promesa
+
+async function miFuncionConPromesa(){
+    return 'saludos con promesa y async';  //cualquier valor devuelto dentro de una funcion sera automaticamente envuelto dentro de una promesa
+}
+
+//miFuncionConPromesa().then(valor => console.log(valor));
+
+//async / await
+async function funcionConPromesaYAwait(){
+    let miPromesa = new Promise( resolver => {
+        resolver('Promesa con await');
+    })
+
+    console.log(await miPromesa);
+}
+
+//funcionConPromesaYAwait();
+
+//orinesas, await, async y setTimeout
+async function funcionConPromesaAwaitTimeout(){
+    let miPromesa = new Promise(resolver => {
+        setTimeout(() => resolver('promesa con await y timeout'), 3000)
+    })
+    console.log(await miPromesa);
+}
+
+funcionConPromesaAwaitTimeout();
---
+++ JavaScript/Leccion15/EstructurasDeControl.py
@@ -0,0 +1,12 @@
+a = 3
+b = 3
+
+if a == b and not(a > 4 and b > 4):
+    print("a es igual a b y ademas a y b son menores a 4")
+elif a < b:
+    print("a es menor que b")
+elif a > b:
+    print("a es mayor que b")
+
+# si p es verdadero entonces ~p es falso
+# si p es falso entonces ~p es verdadero
